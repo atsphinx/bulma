a3bb9d3680a4075cdf344ba68697a464d59ba243
Kazuya Takei <myself@attakei.net>
2025-10-25 22:59:21 +0900

    feat: Apply menu-list filter to globaltoc

--- tests/test_toc.py
+++ tests/test_toc.py
@@ -19,3 +19,22 @@ def test__added_attr_in_toc(app: SphinxTestApp, status: StringIO, warning: Strin
     soup = BeautifulSoup((app.outdir / "index.html").read_text(), "html.parser")
     toc_h3 = soup.find(lambda tag: tag.name == "h3" and tag.text == "Table of Contents")  # type: ignore[union-attr]
     assert "menu-list" in toc_h3.parent.find("ul").attrs["class"]  # type: ignore[union-attr]
+
+
+@pytest.mark.sphinx(
+    "html",
+    confoverrides={
+        "html_theme": "bulma-basic",
+        "html_sidebars": {
+            "**": ["sidebar/globaltoc.html"],
+        },
+    },
+    testroot="toc",
+)
+def test__added_attr_in_globaltoc(
+    app: SphinxTestApp, status: StringIO, warning: StringIO
+):
+    app.build()
+    soup = BeautifulSoup((app.outdir / "index.html").read_text(), "html.parser")
+    toc_h3 = soup.find(lambda tag: tag.name == "h3" and tag.text == "Table of Contents")  # type: ignore[union-attr]
+    assert "menu-list" in toc_h3.parent.find("ul").attrs["class"]  # type: ignore[union-attr]
